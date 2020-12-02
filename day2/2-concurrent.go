b4a6ac93803b19213bc94bc8c6aa16ed1879f1c0
Scott Ratigan <36186544+scotteratigan@users.noreply.github.com>
2020-12-02 09:49:27 -0800

    Create 2-concurrent.go

---
+++ day2/2-concurrent.go
@@ -0,0 +1,64 @@
+// Re-implementation of 2.go using concurrency
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strconv"
+	"strings"
+	"sync"
+	"sync/atomic"
+)
+
+func main() {
+	var validPassCount uint64
+	var wg sync.WaitGroup
+	lines := readFileIntoStrArr("passwords.txt", "\n")
+	fullRegex := regexp.MustCompile(`^(\d+)-(\d+) (\w): (\w+)$`)
+	for _, line := range lines {
+		wg.Add(1)
+		go func(l string) {
+			if isValidPassword(l, fullRegex) {
+				atomic.AddUint64(&validPassCount, 1)
+			}
+			wg.Done()
+		}(line)
+	}
+	wg.Wait()
+	fmt.Println("Valid passwords:", validPassCount)
+}
+
+func isValidPassword(line string, reg *regexp.Regexp) (bool) {
+	matchArr := reg.FindStringSubmatch(line)
+	firstIndex := toInt(matchArr[1]) - 1 // -1 because input is 1-indexed but strings are 0-indexed
+	secondIndex := toInt(matchArr[2]) - 1
+	char := matchArr[3]
+	pw := matchArr[4]
+	matches := 0
+	if string(pw[firstIndex]) == char {
+		matches += 1
+	}
+	if string(pw[secondIndex]) == char {
+		matches += 1
+	}
+	return matches == 1 // only 1 char should match, not 0 or 2
+}
+
+func toInt(input string) int {
+	num, err := strconv.Atoi(input)
+	if err != nil {
+		panic("Unable to convert a number, aborting.")
+	}
+	return num
+}
+
+func readFileIntoStrArr(fileName, separator string) []string {
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+	lines := strings.Split(string(data), separator)
+	return lines
+}
